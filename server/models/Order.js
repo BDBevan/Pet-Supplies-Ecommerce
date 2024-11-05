a30caffcba14e5e974ec171c8a03cee32e85c21c
Voyager <bradley.bevan@gmail.com>
2024-11-05 20:32:57 +1100

    initial mongo DB configuration

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,3 +1,34 @@
 const User = require('./User');
 
 module.exports = { User };
+
+
+// models/Order.js
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Order extends Model {}
+
+Order.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    order_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'order',
+  }
+);
+
+module.exports = Order;
