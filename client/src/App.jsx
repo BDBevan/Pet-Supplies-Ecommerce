b9328f6142eeed3928d4c94a1b32c2cdffc5fb92
Jovana667 <jovanavukovic4@hotmail.com>
2024-11-07 20:34:45 +1100

    Add footer component and scroll-to-top button; enhance layout and styling in App and Collections pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import AppNavbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <AppNavbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
